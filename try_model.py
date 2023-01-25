a38c0e92d98ae1c6f95d8ddbd8be9dac7edcf5bb
Noa Benayoun <noabenayoun1998@gmail.com>
2023-01-25 21:13:44 +0200

    model adding new user

--- try_model.py
+++ try_model.py
@@ -4,14 +4,6 @@ from scipy.spatial import distance
 
 pd.options.mode.chained_assignment = None
 
-def calculate_distance(user1, user2):
-    """
-    Calculate the euclidean distance between two users
-    :param user1: first user's data
-    :param user2: second user's data
-    :return: euclidean distance between the two users
-    """
-    return distance.euclidean(user1, user2)
 
 def calculate_distance(user1, user2):
     """
@@ -21,17 +13,6 @@ def calculate_distance(user1, user2):
     :return: euclidean distance between the two users
     """
     return distance.euclidean(user1, user2)
-
-def select_random_sample(df, n):
-    """
-    Select a random sample of n rows from the given dataframe
-    :param df: dataframe to select the sample from
-    :param n: number of rows to select
-    :return: dataframe with the selected rows
-    """
-    random_rows = random.sample(range(len(df)), n)
-    df_new_sample = df.iloc[random_rows]
-    return df_new_sample
 
 
 def find_closest_users(df, n_closest_users, threshold, group_number):
@@ -54,7 +35,8 @@ def find_closest_users(df, n_closest_users, threshold, group_number):
             min_diff = float('inf')
             closest_user = None
             for i, row in df[df['Unnamed: 0'] != starting_user].iterrows():
-                user1 = df.loc[df['Unnamed: 0'] == starting_user][df.columns.difference(['Unnamed: 0', 'group'])].values.flatten()
+                user1 = df.loc[df['Unnamed: 0'] == starting_user][
+                    df.columns.difference(['Unnamed: 0', 'group'])].values.flatten()
                 user2 = row[df.columns.difference(['Unnamed: 0', 'group'])].values
                 diff = calculate_distance(user1, user2)
                 if diff < min_diff:
@@ -79,23 +61,74 @@ def find_closest_users(df, n_closest_users, threshold, group_number):
     return group_number, closest_users, df
 
 
+def best_topic(index_list, df):
+    index_df = df.loc[index_list]
+    index_df = index_df.drop(columns=['Age', 'Gender_mapped', 'Unnamed: 0'])
+    sums = index_df.sum()
+    max_col = sums.idxmax()
+    return max_col
+
+
+def new_user_potential_group(row_sample, dict_group, df_new, df, threshold):
+    distance_within_threshold = dict()
+
+    for group, value in dict_group.items():
+        if len(value[0]) < 6:
+            index_df = df.loc[value[0]]
+            min_diff = float('inf')
+            for i, row in index_df.iterrows():
+                new_user = row_sample[df_new.columns.difference(['Unnamed: 0', 'group'])].values.flatten()
+                old_user = row[df.columns.difference(['Unnamed: 0', 'group'])].values
+                diff = calculate_distance(new_user, old_user)
+                if diff < threshold:
+                    if diff < min_diff:
+                        min_diff = diff
+                        distance_within_threshold.update({group: min_diff})
+
+    return distance_within_threshold
+
+
+def min_distance_or_new_group(new_value_dict_dist, existing_group, index_new_user):
+    if new_value_dict_dist:
+        min_key = min(new_value_dict_dist, key=lambda x: new_value_dict_dist[x])
+        existing_group[min_key][0].append(index_new_user[0])
+    else:
+        existing_group[max(existing_group.keys()) + 1] = [[index_new_user[0]]]
+    return existing_group
+
+
+def group_without_topic(dict_value, df):
+    for value in dict_value.values():
+        if len(value) == 1:
+            topic = best_topic(value[0], df)
+            value.append(topic)
+    return dict_value
+
+
 if __name__ == '__main__':
     # read the data
     df = pd.read_csv('lingo_data.csv')
     # select a random sample of 30 rows
-    df_sample = select_random_sample(df, 40)
+    df_sample = df.sample(n=30)
+    df_new = df.drop(df_sample.index)
+
+    row_sample = None
+    #row_sample = df_new.sample()
 
     THRESHOLD = 2
     n_closest_users = 6
 
     group_number = 0
+    dict_group = dict()
 
     while len(df_sample) > 1:
         group_number, closest_users, df_sample = find_closest_users(df_sample, n_closest_users, THRESHOLD, group_number)
+        topic = best_topic(list(user[0] for user in closest_users), df)
+        dict_group.update({group_number: [[user[0] for user in closest_users], topic]})
 
-        print(closest_users)
+    if row_sample is not None:
+        distance_within_threshold = new_user_potential_group(row_sample, dict_group, df_new, df, THRESHOLD)
+        dict_group = min_distance_or_new_group(distance_within_threshold, dict_group, row_sample.index)
+        dict_group = group_without_topic(dict_group, df)
 
-        print("Group number:", group_number)
-        print("User ids:", [user[0] for user in closest_users])
-
-
+    print(dict_group)
