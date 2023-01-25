fc1822077391fa8db000d7b8b3a813358dbcbf1f
BenSiv <bensiv92@gmail.com>
2023-01-25 16:59:50 +0200

    server fixes

--- server/inference.py
+++ server/inference.py
@@ -3,7 +3,7 @@ inference server
 """
 
 # moduls
-from test_server import greet_df
+from test_server import greet_sf
 from flask import Flask
 from flask import request
 
@@ -13,7 +13,7 @@ def main():
 
     @app.route("/get_chat_id")
     def get_chat_room():
-        return greet_df()
+        return greet_fs()
 
     app.run()
 
