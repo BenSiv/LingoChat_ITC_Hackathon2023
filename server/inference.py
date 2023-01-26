a540f233a057c0aefb0aeadc12ee56fc7f4414c4
BenSiv <bensiv92@gmail.com>
2023-01-26 12:12:55 +0200

    server bugs

--- server/inference.py
+++ server/inference.py
@@ -20,14 +20,18 @@ def main():
 
     @app.route("/get_chat_id")
     def get_chat_id():
-
+        print("start")
         con = create_connection()
 
         feature_inputs = request.args.to_dict()
+        print(feature_inputs)
         # example:
         # feature_inputs = {"user_id" : 823, "language" : "english", "max_users" : 6}
-        user_vector = get_user_vector(feature_inputs["user_id"], con)
-        available_chats = get_available_chats(feature_inputs["max_users"], feature_inputs["language"], con)
+        max_users = int(feature_inputs["max_users"])
+        user_id = int(feature_inputs["user_id"])
+
+        user_vector = get_user_vector(user_id, con)
+        available_chats = get_available_chats(max_users, feature_inputs["language"], con)
         for user_number, chat_rooms in available_chats.items():
             current_chat_vectors = get_chat_vectors(chat_rooms, con)
             distances = np.array(distance_vectorized(current_chat_vectors, user_vector))
@@ -39,14 +43,16 @@ def main():
 
     @app.route("/update_chat_vector")
     def update_chat_vector():
-
+        print("start")
         con = create_connection()
 
         inputs = request.args.to_dict()
         # example:
         # inputs = {"chat_id" : 450}
-        rating_vector = calc_chat_vector(inputs["chat_id"], con)
-        update_chat_vector(inputs["chat_id"], rating_vector, con)
+        chat_id = int(inputs["chat_id"])
+        print(inputs)
+        rating_vector = calc_chat_vector(chat_id, con)
+        update_chat_vector(chat_id, rating_vector, con)
 
 
     app.run(host='0.0.0.0', port=8080)
